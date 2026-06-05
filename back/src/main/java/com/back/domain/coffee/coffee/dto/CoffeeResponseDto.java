c46614ba53365fe798b5ce4f292fedfbe05784de
devho <jshgold93@gmail.com>
2026-06-05 21:41:33 +0900

    커피관련 반환DTO 유효값 검사 추가

--- back/src/main/java/com/back/domain/coffee/coffee/dto/CoffeeResponseDto.java
+++ back/src/main/java/com/back/domain/coffee/coffee/dto/CoffeeResponseDto.java
@@ -1,12 +1,17 @@
 package com.back.domain.coffee.coffee.dto;
 
 import com.back.domain.coffee.coffee.entity.Coffee;
+import jakarta.validation.constraints.NotNull;
 
 public class CoffeeResponseDto {
     public record CoffeeResponse(
+            @NotNull
             Long id,
+            @NotNull
             String name,
+            @NotNull
             Integer price,
+            @NotNull
             Integer stock,
             String imgUrl) {}
 
