7f0afe9a4c37b9f3fa5cc662ec2886d73d8a8433
devho <jshgold93@gmail.com>
2026-06-05 21:24:11 +0900

    주문정보반환DTO 유효값 추가및 배송여부 필드 추가된 record 신규작성

--- back/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
+++ back/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
@@ -1,12 +1,41 @@
 package com.back.domain.order.order.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record OrderResponseDto(Long id,
-                               String email,
-                               String address,
-                               List<OrderItemResponseDto> orderItems, // 응답용 상세 DTO 리스트
-                               Integer totalPrice,                    // 총 결제 금액
-                               LocalDateTime createdAt) {
+public class OrderResponseDto {
+    public record OrderResponse(
+            @NotNull
+            Long id,
+            @NotNull
+            String email,
+            @NotNull
+            String address,
+            @NotEmpty
+            List<OrderItemResponseDto> orderItems, // 응답용 상세 DTO 리스트
+            @NotNull
+            Integer totalPrice,                    // 총 결제 금액
+            @NotNull
+            LocalDateTime createdAt) {}
+
+    public record OrderWithIsShippedResponse(
+            @NotNull
+            Long id,
+            @NotNull
+            String email,
+            @NotNull
+            String address,
+            @NotEmpty
+            List<OrderItemResponseDto> orderItems, // 응답용 상세 DTO 리스트
+            @NotNull
+            Integer totalPrice,                    // 총 결제 금액
+            @NotNull
+            Boolean isShipped,
+            @NotNull
+            LocalDateTime createdAt) {}
 }
+
+
