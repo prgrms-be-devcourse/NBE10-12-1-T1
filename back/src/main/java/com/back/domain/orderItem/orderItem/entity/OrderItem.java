9677239a4f52974bf5734ad2aa404bfde8bebcad
puppywimy <eric250@naver.com>
2026-06-07 01:49:26 +0900

    feat: OrderItem 엔티티에 ERD에 맞게 필드(amount, order, coffee) 추가

--- back/src/main/java/com/back/domain/orderItem/orderItem/entity/OrderItem.java
+++ back/src/main/java/com/back/domain/orderItem/orderItem/entity/OrderItem.java
@@ -1,8 +1,16 @@
 package com.back.domain.orderItem.orderItem.entity;
 
+import com.back.domain.coffee.coffee.entity.Coffee;
+import com.back.domain.order.order.entity.Order;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class OrderItem extends BaseEntity {
+    private int amount;
+    @ManyToOne
+    private Order order;
+    @ManyToOne
+    private Coffee coffee;
 }
