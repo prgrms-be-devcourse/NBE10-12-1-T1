452074d8ac65ab45475b5f68c0f9d71012db5a2c
puppywimy <eric250@naver.com>
2026-06-07 01:11:53 +0900

    feat: Delivery 엔티티에 ERD에 맞게 필드(isShipped, order) 추가

--- back/src/main/java/com/back/domain/delivery/delivery/entity/Delivery.java
+++ back/src/main/java/com/back/domain/delivery/delivery/entity/Delivery.java
@@ -1,8 +1,13 @@
 package com.back.domain.delivery.delivery.entity;
 
+import com.back.domain.order.order.entity.Order;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Delivery extends BaseEntity {
+    private boolean isShipped;
+    @OneToOne
+    private Order order;
 }
