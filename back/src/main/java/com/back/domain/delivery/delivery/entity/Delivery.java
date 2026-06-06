03b30f367d0644c519dfa32f0519ef947de0398c
puppywimy <74778253+puppywimy@users.noreply.github.com>
2026-06-07 02:27:14 +0900

    [#12] Delivery Entity, Repository, Service 추가 (#40)
    * feat: Delivery 엔티티에 ERD에 맞게 필드(isShipped, order) 추가
    
    * feat: Getter와 NoArgsConstructor 어노테이션 추가

--- back/src/main/java/com/back/domain/delivery/delivery/entity/Delivery.java
+++ back/src/main/java/com/back/domain/delivery/delivery/entity/Delivery.java
@@ -1,8 +1,17 @@
 package com.back.domain.delivery.delivery.entity;
 
+import com.back.domain.order.order.entity.Order;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Delivery extends BaseEntity {
+    private boolean isShipped;
+    @OneToOne
+    private Order order;
 }
