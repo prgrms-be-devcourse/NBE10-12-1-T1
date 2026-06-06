17b0223731af509ec906c9c76a79ad61628e74bc
oxcm07 <oxcm0727@gmail.com>
2026-06-07 01:59:14 +0900

    feat/13: GET /admin/orders 구현 (#35)
    * feat: GET /admin/orders 구현
    
    * fix: 객체타입 명시 및 Coffee칼럼 Join

--- back/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
+++ back/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
@@ -1,12 +1,38 @@
 package com.back.domain.order.order.dto;
 
+import com.back.domain.order.order.entity.Order;
+import com.back.domain.orderItem.orderItem.entity.OrderItem;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public record OrderResponseDto(Long id,
                                String email,
                                String address,
-                               List<OrderItemResponseDto> orderItems, // 응답용 상세 DTO 리스트
-                               Integer totalPrice,                    // 총 결제 금액
+                               List<OrderItemResponseDto> orderItems,
+                               Integer totalPrice,
                                LocalDateTime createdAt) {
+    public static OrderResponseDto from(Order order) {
+        List<OrderItemResponseDto> items = new ArrayList<>();
+        int totalPrice = 0;
+
+        for(OrderItem orderItem : order.getOrderItems()) {
+            items.add(new OrderItemResponseDto(
+                    orderItem.getCoffee().getName(),
+                    orderItem.getAmount(),
+                    orderItem.getPrice()
+            ));
+            totalPrice += orderItem.getPrice() * orderItem.getAmount();
+        }
+
+        return new OrderResponseDto(
+                (long) order.getId(),
+                order.getEmail(),
+                order.getAddress(),
+                items,
+                totalPrice,
+                order.getCreatedAt()
+        );
+    }
 }
--- back/src/main/java/com/back/domain/orderItem/orderItem/entity/OrderItem.java
+++ back/src/main/java/com/back/domain/orderItem/orderItem/entity/OrderItem.java
@@ -1,8 +1,28 @@
 package com.back.domain.orderItem.orderItem.entity;
 
+import com.back.domain.coffee.coffee.entity.Coffee;
+import com.back.domain.order.order.entity.Order;
 import com.back.global.jpa.entity.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "OrderItem")
+@Getter
+@NoArgsConstructor
 public class OrderItem extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coffeeId")
+    private Coffee coffee;
+
+    @Column
+    private int amount;
+
+    @Column
+    private int price;
 }
