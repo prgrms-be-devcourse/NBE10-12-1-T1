cd28bcce8c1919bc61aa69e5e2cd77bd2891cd3c
devho <jshgold93@gmail.com>
2026-06-07 17:21:53 +0900

    CoffeeService 화면 주석으로 코멘트 남기기 헤헤..

--- back/src/main/java/com/back/domain/coffee/coffee/service/CoffeeService.java
+++ back/src/main/java/com/back/domain/coffee/coffee/service/CoffeeService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+//@Transactional 이걸 쓸까요? 말까요? 쓰면 메소스단에 단순 Transactional은 제거가능
 public class CoffeeService {
     private final CoffeeRepository coffeeRepository;
 
@@ -24,7 +25,7 @@ public class CoffeeService {
 
     }
 
-
+    //해당 메소드의 기능은 뭐일까요? 테스트용도일까요?
     @Transactional(readOnly = true)
     public Optional<Coffee> findLatest() {
         return coffeeRepository.findFirstByOrderByIdDesc();
@@ -39,6 +40,11 @@ public class CoffeeService {
             String imgUrl
     ) {
         final Coffee coffee = new Coffee(name, price, stock, imgUrl);
+        // 흐음~ entity를 바로 넘기는건 위험하다고 하네요
+        // 저희 수업시간에 post & comment처럼 서로 참조하는 상황일 때
+        // 화면에 계속 연쇄적으로 붙어서 출력된 거 기억하실까요? 그런 문제가 발생 할 수 도있고
+        // DB값이 그대로 화면으로 노출 될 위험이 있기에 Service단에서 포장까지 다 하고
+        // Controller로 넘기는게 더 안전하다고하네용?? 허헣 덕분에 배워갑니다.
         return coffeeRepository.save(coffee);
     }
 }
