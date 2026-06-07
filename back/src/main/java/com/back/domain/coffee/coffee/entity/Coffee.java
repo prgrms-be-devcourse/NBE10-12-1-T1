c5fd24905dbfd76445dc85b095bb8f3c10c42015
puppywimy <74778253+puppywimy@users.noreply.github.com>
2026-06-07 14:53:07 +0900

    [#66] [관리자 페이지] POST /admin/coffees - 상품 추가 (#43)
    * test: Post /admin/coffees 테스트 작성 및 관련 코드 추가
    
    * feat: POST /admin/coffees 구현(TempResponseDto를 임시로 정의, Coffee 엔티티 수정)
    
    * fix: CoffeeService의 각 메소드에서 Transactional로 설정

--- back/src/main/java/com/back/domain/coffee/coffee/entity/Coffee.java
+++ back/src/main/java/com/back/domain/coffee/coffee/entity/Coffee.java
@@ -3,12 +3,14 @@ package com.back.domain.coffee.coffee.entity;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Coffee extends BaseEntity {
     @Column(nullable = false)
     private String name;
@@ -17,8 +19,8 @@ public class Coffee extends BaseEntity {
     private int price;
 
     @Column(nullable = false)
-    private String imgUrl;
+    private int stock;
 
     @Column(nullable = false)
-    private int stock;
+    private String imgUrl;
 }
--- back/src/main/java/com/back/domain/coffee/coffee/service/CoffeeService.java
+++ back/src/main/java/com/back/domain/coffee/coffee/service/CoffeeService.java
@@ -1,20 +1,21 @@
 package com.back.domain.coffee.coffee.service;
 
 import com.back.domain.coffee.coffee.dto.CoffeeResponseDto;
+import com.back.domain.coffee.coffee.entity.Coffee;
 import com.back.domain.coffee.coffee.repository.CoffeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional( readOnly = true)
 public class CoffeeService {
-
     private final CoffeeRepository coffeeRepository;
 
+    @Transactional(readOnly = true)
     public List<CoffeeResponseDto> getCoffees() {
         return coffeeRepository.findAll()
                 .stream()
@@ -23,4 +24,21 @@ public class CoffeeService {
 
     }
 
+
+    @Transactional(readOnly = true)
+    public Optional<Coffee> findLatest() {
+        return coffeeRepository.findFirstByOrderByIdDesc();
+    }
+
+
+    @Transactional
+    public Coffee create(
+            String name,
+            int price,
+            int stock,
+            String imgUrl
+    ) {
+        final Coffee coffee = new Coffee(name, price, stock, imgUrl);
+        return coffeeRepository.save(coffee);
+    }
 }
