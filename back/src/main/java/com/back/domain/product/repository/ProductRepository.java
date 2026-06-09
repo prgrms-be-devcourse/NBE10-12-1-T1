f306d786bb6169b05d193d25ce647388f2c82771
oxcm07 <oxcm0727@gmail.com>
2026-06-09 19:18:20 +0900

    fix/132/상품 목록 조회시 소프트 삭제 여부 판단 구현

--- back/src/main/java/com/back/domain/product/repository/ProductRepository.java
+++ back/src/main/java/com/back/domain/product/repository/ProductRepository.java
@@ -3,8 +3,11 @@ package com.back.domain.product.repository;
 import com.back.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findFirstByOrderByIdDesc();
+
+    List<Product> findByDeletedAtIsNull();
 }
