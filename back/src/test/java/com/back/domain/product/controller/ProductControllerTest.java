09397cfbb2d29338903e34ec9417ef932fc55c8f
oxcm07 <oxcm0727@gmail.com>
2026-06-09 14:46:12 +0900

    feat/123/[베스트 케이스 테스트 코드 작성] 상품 목록 조회 MockMvc 변경

--- back/src/test/java/com/back/domain/product/controller/ProductControllerTest.java
+++ back/src/test/java/com/back/domain/product/controller/ProductControllerTest.java
@@ -1,18 +1,15 @@
 package com.back.domain.product.controller;
 
 
-import com.back.domain.product.dto.ProductResponseDto;
-import com.back.domain.product.service.ProductService;
+import com.back.domain.order.order.repository.OrderRepository;
+import com.back.domain.product.entity.Product;
+import com.back.domain.product.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,21 +17,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerTest {
+    private final MockMvc mockMvc;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    MockMvc mockMvc;
-    @MockitoBean
-    ProductService productService;
+    public ProductControllerTest(MockMvc mockMvc, OrderRepository orderRepository, ProductRepository productRepository) {
+        this.mockMvc = mockMvc;
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+    }
 
     @Test
-    @DisplayName("GET /products - 상품 목록 조회 성공")
+    @DisplayName("상품 목록 조회 성공")
     void t1() throws Exception {
-        List<ProductResponseDto> mockData = List.of(
-                new ProductResponseDto(1L, "맛있는 원두", 30000, 100, "coffee1.jpg"),
-                new ProductResponseDto(2L, "더 맛있는 원두", 45000, 200, "coffee2.jpg")
+        Product product1 = productRepository.save(
+                new Product("맛있는 원두", 30000, 100, "coffee1.jpg")
         );
 
-        given(productService.getProducts()).willReturn(mockData);
+        Product product2 = productRepository.save(
+                new Product("더 맛있는 원두", 45000, 200, "coffee2.jpg")
+        );
 
         mockMvc.perform(get("/api/v1/products"))
                 .andExpect(status().isOk())
@@ -43,13 +46,13 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.data").isArray())
                 .andExpect(jsonPath("$.data.length()").value(2))
 
-                .andExpect(jsonPath("$.data[0].id").value(1))
+                .andExpect(jsonPath("$.data[0].id").value(product1.getId()))
                 .andExpect(jsonPath("$.data[0].name").value("맛있는 원두"))
                 .andExpect(jsonPath("$.data[0].price").value(30000))
                 .andExpect(jsonPath("$.data[0].stock").value(100))
                 .andExpect(jsonPath("$.data[0].imgUrl").value("coffee1.jpg"))
 
-                .andExpect(jsonPath("$.data[1].id").value(2))
+                .andExpect(jsonPath("$.data[1].id").value(product2.getId()))
                 .andExpect(jsonPath("$.data[1].name").value("더 맛있는 원두"))
                 .andExpect(jsonPath("$.data[1].price").value(45000))
                 .andExpect(jsonPath("$.data[1].stock").value(200))
@@ -57,9 +60,9 @@ public class ProductControllerTest {
     }
 
     @Test
-    @DisplayName("GET /products - 상품이 없으면 빈 배열 반환")
+    @DisplayName("상품 목록 조회 성공 - 빈 배열 반환")
     void t2() throws Exception {
-        given(productService.getProducts()).willReturn(List.of());
+        productRepository.deleteAll();
 
         mockMvc.perform(get("/api/v1/products"))
                 .andExpect(status().isOk())
