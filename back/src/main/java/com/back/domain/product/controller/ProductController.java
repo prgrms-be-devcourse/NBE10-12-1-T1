2eb4fb6332828b3b70aa2555305830f4a8be155c
devho <jshgold93@gmail.com>
2026-06-09 16:28:44 +0900

    feat/74/dev:API 버저닝

--- back/src/main/java/com/back/domain/product/controller/ProductController.java
+++ back/src/main/java/com/back/domain/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.back.domain.product.controller;
 
 import com.back.domain.product.dto.ProductResponseDto;
 import com.back.domain.product.service.ProductService;
+import com.back.global.annotation.ApiV1;
 import com.back.global.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@ApiV1
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
