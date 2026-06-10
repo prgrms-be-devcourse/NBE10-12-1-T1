de2f6c77a7c4d5d59df4ad03948278c52bd9e696
evelman2001-lgtm <evelman2001@gmail.com>
2026-06-10 09:36:15 +0900

    dd

--- back/src/main/java/com/back/domain/product/repository/ProductRepository.java
+++ back/src/main/java/com/back/domain/product/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.back.domain.product.repository;
 import com.back.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
